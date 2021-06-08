763d1ceee25fbcc58b22bdd2e93a24ab9ac4cf90
Jörg Hofmann <joerg@thuenic.de>
2021-06-08 13:18:08 +0200

    WebGui fertig

---
+++ chatbot-webgui.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, flash
+from chatbot import Chatbot
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Pa$$w0rd'
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    chatbot_label = "Hallo, worüber wollen Sie sprechen?"
+
+    # Listen
+    zufallsantworten = ["Oh wirklich...",
+                        "Interessant",
+                        "Das kann man so sehen",
+                        "Ich verstehe"]
+    reaktionen = {"hallo": "aber hallo",
+                  "geht": "was verstehst du darunter?",
+                  "schmeckt": "Ich habe keinen Geschmackssinn."}
+
+    # Logik
+    if request.method == 'POST':
+        chatbot_input = request.form['chatbot_input']
+        if not chatbot_input:
+            flash("Ohne Frage kann ich nicht antworten")
+        else:
+            bot = Chatbot(reaktionen, zufallsantworten)
+            bot.set_Message(chatbot_input)
+            chatbot_label = bot.get_Response()
+
+    return render_template('chatbot.html', chatbot_label=chatbot_label)
